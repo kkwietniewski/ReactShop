63ad60489ef9e317aa5f9b3546fe68ed9e3b0946
unknown <kkwietniewski@edu.cdv.pl>
2020-12-22 23:07:01 +0100

    React basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import  React, {useState}  from "react";
+import Tweet from './Tweet';
 
 function App() {
+
+  const [users, setUsers] = useState([
+    {name: 'Kacper', message: 'Hello'},
+    {name: 'John Snow', message: 'winter'},
+    {name: 'Bran Stark', message: 'boringggg'}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {users.map(user =>(
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
 }
